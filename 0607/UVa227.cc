dc58d0ee8553501ebf46500aa018bbdde9858d3b
fripSide <clangllvm@126.com>
2016-06-10 17:30:49 +0800

    crack the puzzle

--- 0607/UVa227.cc
+++ 0607/UVa227.cc
@@ -1,58 +1,77 @@
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
+#include <ctype.h>
 
 using namespace std;
 
 int main() {
-    char mp[5][5];
-    const int maxn = 200;
+    char mp[5][7];
+    const int maxn = 1005;
     char s[maxn];
-    bool cp = true;
     int idx = 1;
     while (true) {
-        for (int i = 0; i < 5; i++) fgets( mp[i], 5, stdin);
-        if (mp[0][0] == 'Z' && mp[0][1] == 0) break;
-        int x, y;
-        for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 5; ++j) {
-                if (mp[i][j] == ' ')
-                    x = i, y = j;
+        bool end = false;
+        for (int i = 0; i < 5; i++) {
+            fgets(mp[i], 7, stdin);
+            if (mp[0][0] == 'Z') {
+                 end = true;
+                 break;
             }
         }
-        for (int i = 0; i < 5; ++i) printf("%s\n", mp[i]);
-        printf("\n");
-        bool has = true, end = false;
-        while (scanf("%s", s) != EOF) {
-            for (int i = 0; s[i]; ++i) {
-                if (s[i] == '0') {
-                    end = true;
-                    break;
-                }
-                if (s[i] == 'A') {
-                    if (x - 1 > 0) swap(mp[x][y], mp[x - 1][y]);
-                    else has = false;                   
-                } else if (s[i] == 'L') {
-                    if (y - 1 > 0) swap(mp[x][y], mp[x][y - 1]);
-                    else has = false;
-                } else if (s[i] == 'R') {
-                    if (y + 1 < 5) swap(mp[x][y], mp[x][y + 1]);
-                    else has = false;
-                } else {
-                    if (x + 1 < 5) swap(mp[x][y], mp[x + 1][y]);
-                    else has = false;
+        if (end) break;
+        int x, y;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (mp[i][j] == ' ') {
+                    x = i, y = j;
+                    i = j = 5;
                 }
             }
-            if (end) break;
-            if (!has) break;
+        }
+        
+        //memset(s, 0, sizeof s);
+        int p = 0;
+        while (p < maxn) {
+            char tmp = getchar();
+            if (tmp == '0') break;
+            s[p++] = tmp;
+        }
+        s[p] = 0;
+        getchar();
+        bool has = true;    
+        for (int i = 0; i < s[i]; ++i) {
+            int tx = x, ty = y;
+            if (s[i] == 'A') {
+                tx = x - 1;
+            } else if (s[i] == 'L') {
+                ty = y - 1;
+            } else if (s[i] == 'R') {
+                ty = y + 1;
+            }
+            // 这里不能用else，输入可能有其他的字符,例如： \t \n \space等
+            else if (s[i] == 'B'){
+                tx = x + 1;
+            }
+            if (tx < 0 || ty < 0 || tx > 4 || ty > 4) {
+                has = false; break;
+            }
+            mp[x][y] = mp[tx][ty];
+            mp[tx][ty] = ' ';
+            x = tx, y = ty;
         }
         if (idx > 1) printf("\n");
         printf("Puzzle #%d:\n", idx++);
-        if (has) printf("This puzzle has no final configuration.\n");
-        else {
-            for (int i = 0; i < 5; ++i) {
-                printf("%s\n", mp[i]);
+        if (!has) {
+            printf("This puzzle has no final configuration.\n");
+            continue;
+        }
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                if (j) printf(" ");
+                printf("%c", mp[i][j]);
             }
+            printf("\n");
         }
     }
     return 0;
